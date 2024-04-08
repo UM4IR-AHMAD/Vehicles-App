4e7786cd6b88f0d334c23264471ea94580bbd6f7
Umair Ahmad <um4ir.ahmad@gmail.com>
2024-04-08 06:57:03 +0500

    Update list.vue

--- Vehicles-app/src/views/categories/list.vue
+++ Vehicles-app/src/views/categories/list.vue
@@ -108,26 +108,7 @@
     </div>
     <!-- pagination start -->
     <div v-show="categories.last_page > 1">
-      <div class="flex justify-end">
-        <button v-if="categories.current_page > 1" @click="getByPageNo()"
-          class="leading-pro ease-soft-in text-xs bg-150 w-6.35 h-6.35 p-1.2 rounded-3.5xl tracking-tight-soft bg-x-25 mr-4 mb-0 flex cursor-pointer items-center justify-center border border-solid border-slate-700 border-transparent bg-transparent text-center align-middle font-bold uppercase text-slate-700 transition-all hover:opacity-75">
-          <span class="text-xl mb-1 mr-0.5">‹</span>
-        </button>
-        <!-- page links creation -->
-        <template v-for="(link, index) of categories.links" :key="index">
-          <button v-if="link.label >= 1 && link.label <= categories.last_page" @click="getByPageNo(link.label)"
-            class="leading-pro ease-soft-in bg-150 w-6.35 h-6.35 p-1.2 rounded-3.5xl tracking-tight-soft bg-x-25 mr-4 mb-0 flex cursor-pointer items-center justify-center border border-solid  border-transparent bg-transparent text-center align-middle font-bold uppercase  transition-all "
-            :class="[link.active ? 'border-lime-500 cursor-not-allowed' : 'border-slate-700 hover:opacity-75']"
-            :disabled="link.active">
-            <span class="text-sm mr-0.5">{{ link.label }}</span>
-          </button>
-        </template>
-        <!-- end page links creation -->
-        <button v-if="categories.current_page < categories.last_page" @click="getByPageNo(categories.last_page)"
-          class="leading-pro ease-soft-in text-xs bg-150 w-6.35 h-6.35 p-1.2 rounded-3.5xl tracking-tight-soft bg-x-25 mr-4 mb-0 flex cursor-pointer items-center justify-center border border-solid border-slate-700 border-transparent bg-transparent text-center align-middle font-bold uppercase  transition-all hover:opacity-75">
-          <span class="text-xl mb-1 mr-0.5">›</span>
-        </button>
-      </div>
+      <pagination :data="categories" @byPageNo="getByPageNo"/>
     </div>
     <!-- pagination end -->
   </div>
@@ -142,7 +123,9 @@
 import { onMounted, ref } from 'vue';
 import { useGetRequest, useDeleteRequest } from '@/axiosClient';
 import { useRouter } from 'vue-router';
+import pagination from '@/components/pagination.vue';
 
+  
 const router = useRouter();
 
 
